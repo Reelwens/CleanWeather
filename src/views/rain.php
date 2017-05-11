cc7d0f4fb4d19206d064895c5095db9244264841
Reelwens <sim.lucas27@gmail.com>
2017-05-11 02:21:46 +0200

    :lipstick: Rain parallax

---
+++ src/views/rain.php
@@ -0,0 +1,52 @@
+<div id="parallax">
+    
+    <div class="layer rain rain_1" data-depth="0.3">
+        <img src="img/rain_1.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_2" data-depth="0.6">
+        <img src="img/rain_2.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_3" data-depth="0.5">
+        <img src="img/rain_3.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_4" data-depth="0.2">
+        <img src="img/rain_4.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_5" data-depth="0.7">
+        <img src="img/rain_5.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_6" data-depth="0.55">
+        <img src="img/rain_6.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_7" data-depth="0.4">
+        <img src="img/rain_7.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_8" data-depth="0.35">
+        <img src="img/rain_8.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_9" data-depth="0.48">
+        <img src="img/rain_9.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_10" data-depth="0.5">
+        <img src="img/rain_10.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_11" data-depth="0.47">
+        <img src="img/rain_11.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_12" data-depth="0.41">
+        <img src="img/rain_12.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_13" data-depth="0.4">
+        <img src="img/rain_13.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_14" data-depth="0.43">
+        <img src="img/rain_14.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_15" data-depth="0.47">
+        <img src="img/rain_15.svg" alt="Rain">
+    </div>
+    <div class="layer rain rain_16" data-depth="0.48">
+        <img src="img/rain_16.svg" alt="Rain">
+    </div>
+    
+</div>
